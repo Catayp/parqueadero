0bcf10ec8e6b9dca20da1647e8fd0fe62d3839cd
Santiago Andrés Riaño Mogollón <88056527+SantiagoRiano2003@users.noreply.github.com>
2021-11-18 15:28:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <link rel="icon" type="image/x-icon" href="assets/favicon.ico"/>
+        <link rel="icon" type="image/x-icon" href="Vistas\assets\favicon.ico"/>
         <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css"/>
         <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css"/>
