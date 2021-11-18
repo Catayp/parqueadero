e275f9e713efee84f4894d9ebfbc89500d6a207b
Santiago Andrés Riaño Mogollón <88056527+SantiagoRiano2003@users.noreply.github.com>
2021-11-18 15:39:44 -0500

    Update login.php

--- vistas/login.php
+++ vistas/login.php
@@ -1,6 +1,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
+        <link rel="icon" type="image/x-icon" href="assets\favicon.ico"/>
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="">
