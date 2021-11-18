8dc0006a6efac645d885ea6b46ba2f5a8dc18402
Santiago Andrés Riaño Mogollón <88056527+SantiagoRiano2003@users.noreply.github.com>
2021-11-18 15:38:02 -0500

    Update register.php

--- vistas/register.php
+++ vistas/register.php
@@ -1,17 +1,17 @@
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <title>Crear Usuario para ALX</title>
-    <link rel="icon" type="image/x-icon" href="assets/favicon.ico"/>
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-    <link href="css/sb-adminn-2.min.css" rel="stylesheet">
-</head>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>Crear Usuario para ALX</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico"/>
+        <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+            rel="stylesheet">
+        <link href="css/sb-adminn-2.min.css" rel="stylesheet">
+    </head>
 <body class="bg-gradient-primary">
     <div class="container">
         <div class="card o-hidden border-0 shadow-lg my-5">
