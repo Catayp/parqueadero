761f66612fad87902206206aabfba4a0bd0645d7
Santiago Andrés Riaño Mogollón <88056527+SantiagoRiano2003@users.noreply.github.com>
2021-11-21 20:44:19 -0500

    Add files via upload

---
+++ vistas/login.php
@@ -0,0 +1,74 @@
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <link rel="icon" type="image/x-icon" href="assets\favicon.ico"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>Iniciar Sesión en ALX</title>
+        <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+            rel="stylesheet">
+        <link href="css/sb-adminn-2.min.css" rel="stylesheet">
+
+    </head>
+<body class="bg-gradient-primary">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xl-10 col-lg-12 col-md-9">
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <?php if (isset($_GET["mensaje"])) {
+                            if ($_GET["mensaje"]=="error") {
+                        
+                         ?>
+                        <div class="alert alert-danger" role="alert">
+                          Usuario o claves incorrectas
+                        </div>
+                        <?php $_GET["mensaje"]="";
+                        }
+                        } 
+                        ?>
+                        
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">¡TE DAMOS LA BIENVENIDA, ENTRA A TU CUENTA!</h1>
+                                    </div>
+                                    <form class="user" method="POST" action="../controlador/ControladorUsuario.php" >
+                                        <div class="form-group">
+                                            <input type="email" class="form-control form-control-user"
+                                                id="exampleInputEmail" aria-describedby="emailHelp"
+                                                placeholder="Direccion de Correo Electronico" name="email">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control form-control-user"
+                                                id="exampleInputPassword" placeholder="Contraseña" name="clave">
+                                        </div>
+                                        <div class="form-group">
+                                        </div>
+                                        <input type="submit" name="ini" class="btn btn-primary btn-user btn-block" value="Iniciar Sesion">
+                                    </form><br>
+                                    <div class="text-center">
+                                        <a class="small" href="forgot-password.html">¿Olvidaste tu contraseña?</a>
+                                    </div>
+                                    <div class="text-center">
+                                        <a class="small" href="register.php">Crea una Cuenta</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="js/sb-admin-2.min.js"></script>
+</body>
+</html>
